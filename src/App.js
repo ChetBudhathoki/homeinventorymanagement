a251079fc40103cd019f13b38bbf0649590dd476
Chet Budhathoki <okchet@gmail.com>
2019-11-18 18:31:31 -0700

    Added bootstrap link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
